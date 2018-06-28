a0bb79a34b6bcded645ca39029a6fbe4098e49cf
tianajen <tianarakotondramary@gmail.com>
2018-06-28 11:46:06 +0300

    mode p

---
+++ Documents/bettermotherfocking/app.js
@@ -0,0 +1,36 @@
+function popup(){
+    alert("Hey le site xxxvidsxxx est trop bien.Viens dessus stp please!")
+}
+
+setTimeout(popup, 10000)
+
+function back(){
+    document.body.style.backgroundColor = "magenta";
+    var x = document.getElementsByTagName("P");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color = "blue"; 
+        x[i].style.fontFamily = 'papyrus';}
+    var x = document.getElementsByTagName("H1");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color = "green";
+        x[i].style.fontFamily = 'Comic Sans'; }
+        var x = document.getElementsByTagName("H2");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color = "green";
+        x[i].style.fontFamily = 'Comic Sans'; }
+        var x = document.getElementsByTagName("H3");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color = "green";
+        ash2[i].style.fontFamily = 'Comic Sans'; }
+        
+        var x = document.getElementsByTagName("H4");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color = "green"; }
+        ash2[i].style.fontFamily = 'Comic Sans';
+        
+}
